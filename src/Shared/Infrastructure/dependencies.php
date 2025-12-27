c2d9f20bfb39018694e259b14ba510c521552c4c
jorge <jlizancandela@gmail.com>
2025-12-27 13:03:13 +0100

    use namespace imports in dependencies

--- src/Shared/Infrastructure/dependencies.php
+++ src/Shared/Infrastructure/dependencies.php
@@ -1,11 +1,21 @@
 <?php
 
+/**
+ * Simple dependency injection container that initializes all services and repositories used across the app.
+ */
+
 use Latte\Engine;
 use Shared\Infrastructure\Database\Database;
 use Shared\Infrastructure\Email\EmailService;
 use Usuarios\Infrastructure\UserRepository;
 use Usuarios\Application\UserService;
 use Usuarios\Application\AuthService;
+use Servicios\Infrastructure\ServicioRepository;
+use Servicios\Application\ServicioService;
+use Especialistas\Infrastructure\EspecialistaRepository;
+use Especialistas\Infrastructure\EspecialistaServicioRepository;
+use Reservas\Infrastructure\ReservaRepository;
+use Reservas\Application\ReservaService;
 
 $latte = new Engine();
 $latte->setTempDirectory(__DIR__ . '/../../../temp/cache');
@@ -18,9 +28,9 @@ $userRepository = new UserRepository($db);
 $userService = new UserService($userRepository);
 $authService = new AuthService($userRepository, $userService);
 
-$servicioRepository = new \Servicios\Infrastructure\ServicioRepository($db);
-$servicioService = new \Servicios\Application\ServicioService($servicioRepository);
-$especialistaRepository = new \Especialistas\Infrastructure\EspecialistaRepository($db);
-$especialistaServicioRepository = new \Especialistas\Infrastructure\EspecialistaServicioRepository($db);
-$reservaRepository = new \Reservas\Infrastructure\ReservaRepository($db);
-$reservaService = new \Reservas\Application\ReservaService($reservaRepository);
+$servicioRepository = new ServicioRepository($db);
+$servicioService = new ServicioService($servicioRepository);
+$especialistaRepository = new EspecialistaRepository($db);
+$especialistaServicioRepository = new EspecialistaServicioRepository($db);
+$reservaRepository = new ReservaRepository($db);
+$reservaService = new ReservaService($reservaRepository);
