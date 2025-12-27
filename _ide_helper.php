52d7101a6372c20aeedbe857664dddb27943eaa3
jorge <jlizancandela@gmail.com>
2025-12-27 13:27:47 +0100

    add ide helper and type hints for test keys

---
+++ _ide_helper.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * This file is helping the IDE to understand the global variables and context.
+ * It should not be included in the application execution.
+ */
+
+/** @var \Tests\TestCase $this */
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,13 +9,15 @@ use Mockery;
  * Base TestCase class.
  * @property \PDO|\Mockery\MockInterface $pdo
  * @property \PDOStatement|\Mockery\MockInterface $stmt
- * @property \Reservas\Infrastructure\ReservaRepository $repository
+ * @property \Reservas\Infrastructure\ReservaRepository|\Mockery\MockInterface $repository
+ * @property \Reservas\Application\ReservaService $service
  */
 abstract class TestCase extends BaseTestCase
 {
     protected $pdo;
     protected $stmt;
     protected $repository;
+    protected $service;
 
     protected function tearDown(): void
     {
