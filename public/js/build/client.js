5dde73aed094c566e866c542eb3a3607b3a174f2
jorge <jlizancandela@gmail.com>
2026-01-04 23:02:25 +0100

    chore: stop tracking build artifacts in public/js/build

--- public/js/build/client.js
+++
@@ -1,2 +0,0 @@
-import{a as n,b as s,c as o,d as l}from"./chunk-IYLQV4OL.js";import{a,b as e,e as r,f as i}from"./chunk-N34MI4RW.js";var t=a(()=>{});var d=e(()=>{n();s();l();i();t();window.toast=o;window.bootstrap=r});export default d();
-//# sourceMappingURL=client.js.map
